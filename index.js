679ef5dcf7c75640fc85ab0d3f660209eaabe977
Almishev Nurbol <almishev.01@gmail.com>
2023-02-26 20:02:05 +0600

    post

--- index.js
+++ index.js
@@ -57,11 +57,11 @@ app.post('/aliexpress', async (req, res) => {
     }
 })
 
-app.get('/l688', async (req, res) => {
+app.post('/l688', async (req, res) => {
     try {
         const id = req.query.id
         const response = await axios.get(`https://t-b.ru.com/catalog/1688/product/${id}/#`)
-        
+
         res.html(response.data)
     } catch (error) {
         res.json(error)
